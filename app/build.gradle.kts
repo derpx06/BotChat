4277344427f5049f23c3c2d3ba048484fe9f4b5b
manas1511200 <manaskhairnar1511@gmail.com>
2025-05-02 18:28:08 +0530

    Create README.md in this commit the setting star tto tget stored and persists

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+
+    // Secure Storage
+    implementation ("androidx.security:security-crypto:1.1.0-alpha06")
     // Add Accompanist for ModalBottomSheetLayout
     implementation ("com.google.accompanist:accompanist-navigation-material:0.31.2-alpha")
     // Gson (you may not need this line if you are using Retrofit with the Gson converter)
